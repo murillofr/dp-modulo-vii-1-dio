9cb409b7504959b55e287792e6ccd266c88056e9
Murillo Falcão Rodrigues <murillo.falcao@gmail.com>
2022-07-25 16:34:03 -0300

    Primeiro commit

---
+++ src/main/java/one/dio/dpmodulovii1dio/service/ClienteService.java
@@ -0,0 +1,24 @@
+package one.dio.dpmodulovii1dio.service;
+
+import one.dio.dpmodulovii1dio.model.Cliente;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ * 
+ * @author murillofr
+ */
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
